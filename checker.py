40ff0bbd1b3a22c07c7e5614d7c4dc2e53fa7fcc
checker5965 <archit.checker_ug20@ashoka.edu.in>
2018-12-22 19:32:36 +0530

    Final Script

---
+++ checker.py
@@ -0,0 +1,51 @@
+import time
+from selenium import webdriver as wd
+from selenium.webdriver.common.keys import Keys
+import smtplib
+import email
+import email.encoders 
+import email.mime.text 
+import email.mime.base
+from email.mime.multipart import MIMEMultipart
+from selenium.common.exceptions import NoSuchElementException
+
+driver = wd.Chrome('''PATH FOR CHROME DRIVER''')
+driver.get("https://lms.ashoka.edu.in/Contents/Grades/ViewGrades.aspx")
+
+time.sleep(30)
+
+num = input("Number of Courses (including co-curriculars): ")
+
+courses = {}
+subjects = []
+
+for i in range (num):
+    courses[i] = 0
+    subjects.append(driver.find_element_by_id("ContentPlaceHolder1_lvView_lblCourseName_"+str(i)).text)
+
+flag = 1
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login("'''EMAIL'''", "'''PASSWORD'''")
+
+while(flag):
+    flag = 0
+    for i in range(num):
+        if courses[i] == 0:
+            flag+=1
+            try:
+                grade = driver.find_element_by_id("ContentPlaceHolder1_lvView_lblGrades_"+str(i))
+            except NoSuchElementException:
+                continue
+            html = "<p>Your "+ subjects[i] + " Grade is out.</p>"
+            emailMsg = email.mime.multipart.MIMEMultipart('mixed')
+            emailMsg['Subject'] = 'Grade Out'
+            emailMsg['From'] = 'someText'
+            emailMsg['To'] = 'yourEmail'
+            emailMsg.attach(email.mime.text.MIMEText(html,'html'))
+            server.sendmail("'''EMAIL'''","'''EMAIL'''",emailMsg.as_string())
+            courses[i] = 1
+    time.sleep(5)
+    driver.refresh()
+server.quit()
